5fe5883bf20d4c2290835aa71529a0b363ac92c5
kerem <73038148+3xg3lin@users.noreply.github.com>
2024-08-25 15:38:15 +0300

    Create clamAV.ps1

---
+++ clamAV.ps1
@@ -0,0 +1,25 @@
+# Which user is enable on the system.
+$enabledUsers = @("'Kerem Utku Gül'" , "Ortak" , "ClamAV")
+
+# loop for ClamAV scanner
+foreach ($user in $enabledUsers) {
+    $paths = @(
+    "C:\Users\$user\Desktop",
+    "C:\Users\$user\Documents",
+    "C:\Users\$user\Downloads",
+    "C:\Users\$user\Music",
+    "C:\Users\$user\Pictures",
+    "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup",
+    )
+
+    $escapedPaths = $paths | ForEach-Object { ""$_"" }
+    $allPaths = $escapedPaths -join " "
+    Write-Output "Checking now: " $allPaths
+
+    # date for logfile
+    $dateTime = (Get-Date).ToString("yyyy-MM-dd_HH-mm-ss")
+    $logFile = "C:\Users\ClamAV\Documents\$user-$path-clamscan-$dateTime.log"
+        
+    clamscan --recursive --remove --log=$logFile $allPaths
+
+}
